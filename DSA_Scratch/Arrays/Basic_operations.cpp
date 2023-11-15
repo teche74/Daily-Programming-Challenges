09d5caaeefcad118f3ef480649dffb5da104803d
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2023-11-15 11:06:24 +0530

    Update Basic_operations.cpp

--- DSA_Scratch/Arrays/Basic_operations.cpp
+++ DSA_Scratch/Arrays/Basic_operations.cpp
@@ -2,92 +2,167 @@
 
 using namespace std;
 
-void input_module(int static_array[ ], vector<int>&dynamic_array, int size){
+void insert_static(int * arr, int ele, int * size){
+    int temp = (*size)++;
 
-    // static array always send as address refrence in calling function.
-    // for dynamic array e have to send the refernce otherwise changes takes place for temporary basis.
-    
+    arr[temp]=ele;
+}
+
+void insert_dynamic(vector<int>&temp ,int ele ){
+
+    temp.push_back(ele);
+}
+
+void traversal_dynamic( vector<int>temp){
+    for(char ch : temp){
+        cout<<ch<<" ";
+    }
+}
+
+void traversal_static(int arr[ ], int size){
     for(int i=0;i<size;i++){
-        cin>>static_array[i];        // taking input from user 
-        
-        
-        int x;
-        cin>>x;
-        dynamic_array.push_back(x);        // for every input it doubles its capacity if it reaches its available size;
-    } 
-
+        cout<<arr[i]<<" ";
+    }
 }
 
-void insert_in_between(int static_array[ ], vector<int>&dynamic_array, int position, int element, int * size){
-
-    // for static array;
-    
-    
-    (*size)++;
-    // shifting element one step forward
-    for(int i=(*size)-1;i>=position-1;i--){
-        static_array[i+1] =static_array[i];
-    }
-    //  new element is placed on 0 based indexing
-    static_array[position-1]= element; 
-
-
-    // for dynamic array;
-    
-
-    // shifting element one step forward
-    int n = dynamic_array.size();
-    dynamic_array.resize(n+1);
-    for(int i=n;i>=position-1;i--){
-        dynamic_array[i+1] =dynamic_array[i];
-    }
-
-    //  new element is placed on 0 based indexing
-    dynamic_array[position-1]= element; 
-}
-
-
-void delete_position_value(int static_array[ ], vector<int>&dynamic_array, int position, int * size){
-
-    // for static array
+void delete_static(int arr[] , int * size){
     (*size)--;
-    // shifting element one step backward
-    for(int i=position-1;i<(*size);i++){
-        static_array[i] =static_array[i+1];
-    }
-    
-    
-    // for dynamic array;
-    // shifting element one step backward
-    int n = dynamic_array.size();
-    dynamic_array.resize(n+1);
-    for(int i=position-1;i<n;i++){
-        dynamic_array[i] =dynamic_array[i+1];
-    }
 
-    dynamic_array.resize(n-1);
+}
+
+
+void delete_dynamic(vector<int>& t){
+    t.pop_back();
+}
+
+void rotation_static(int arr[ ], int size, string direction){
+    if(direction == "left"){
+        int temp = arr[0];
+        for(int i=1;i<size;i++){
+            arr[i-1] = arr[i];
+        }
+        arr[size-1] = temp;
+    }
+    else{
+        int temp = arr[size-1];
+        for(int i=size-2;i>=0;i--){
+            arr[i+1] = arr[i];
+        }
+        arr[0] = temp;
+    }
+}
+
+void dynamic_rotation(vector<int>& temp, string direction){
+    if(direction == "left"){
+        int x = temp.front();
+        for(int i=1;i<temp.size();i++){
+            temp[i-1] =temp[i];
+        }
+        temp[temp.size()-1] =x ; 
+    }
+    else{
+        int x = temp.back();
+        for(int i=temp.size()-2;i>=0;i--){
+            temp[i+1] = temp[i];
+        }
+        temp[temp.size()-1] =x ; 
+    }
+}
+
+void Linear_search_static(int arr[ ], int size, int x){
+    for(int i=0;i<size;i++){
+        if(arr[i] == x){
+            cout<<"Found";
+            return;
+        }
+    }
+    cout<<"Not Found";
+}
+
+void Linear_search_dynamic(vector<int>temp,int x){
+    for(int t : temp){
+        if(t ==x){
+            cout<<"Found";
+            return;
+        }
+    }
+    cout<<"Not Found";
+}
+
+void Bin_srch(vector<int>temp, int ele){
+    int low =0 ,high = temp.size()-1;
+
+    while(low<high){
+        int mid = low+(high-low)/2;
+
+        if(temp[mid] == ele){
+            cout<<"Found";
+            return;
+        }
+
+        else if(temp[mid] > ele){
+            high = mid-1;
+        }
+        else{
+            low =mid+1;
+        }
+    }  
+
+    cout<<"Not Found";
+}
+
+int gcd(int a, int b){
+    return a == 0  ? b : gcd(b%a, b ); 
+}
+
+void Juggling_Algo(vector<int> & t, int size, int rotations){
+    int d ;
+    for(int i=0;i<gcd(rotations,size);i++){
+        int j = i ;
+        int temp =t[i];
+
+        while(true){
+            d= (j+rotations)%size;
+
+            if(d == i) break;
+
+            t[j] = t[d];
+            j =d;
+        }
+        t[j] = temp;
+    }
 }
 
 int main(){
-    int arr[10];            //static array with fixed size..
-    vector<int>a;           // dynamic array with variable size..
+    int size =5;
+    
+    //  defining static arrays
+    int arr[size];
+    int arr2[size] = {1,2,3,4,5};
 
-    int n;                  //defining size for input.
+    // defining dynamic arrays
 
-    cin>>n;  // enter input size
+    vector<int> t;
+    vector<int> x = {1,2,3,4,5};
+    vector<int> z(5,0); // (size, element);
 
-    input_module(arr, a, n);
+    // get length of dynamic arrays;
 
-    int pos,ele;
-
-    cout<<"Enter position and value to insert (Note it works on 0 based indexing )";
-    cin>>pos>>ele;
-    insert_in_between(arr,a,pos,ele,&n);
+    int len = t.size();
 
 
+    // Insertion
+    insert_static(arr2,10,&size);
 
-    cout<<"Enter index position you want to remove value :";
-    cin>>pos;
+    insert_dynamic(t,13);
 
-    delete_position_value(arr,a,pos,&n);
+    // deletion
+    delete_static(arr2,&size);
+    delete_dynamic(t);
+
+    // traversal
+    traversal_static(arr2,size);
+
+    // rotations
+    rotation_static(arr2,size, "left");
 }
