de8e2b5944d54ec86dd715d8e87e8fa88603d76f
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2023-11-22 05:10:41 +0530

    Create LC121_Best_Time_To_Buy_and_Sell.cpp

---
+++ Practise_Sheets/Leetcode/Arrays/LC121_Best_Time_To_Buy_and_Sell.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mn =prices[0];
+        int profit =0;
+
+        for(int i=0;i<prices.size();i++){
+            int cost = prices[i] - mn;
+            profit = max(cost, profit);
+            mn = min(mn, prices[i]);
+        }
+        return profit;
+    }
+};
