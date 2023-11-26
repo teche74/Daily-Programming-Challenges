7dbc2dfb22fe724cebb3b59a12ec3225147a38b6
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2023-11-26 08:53:49 +0530

    Create CN_Letter_Combination_of_a_Phone_Number.cpp

---
+++ Practise_Sheets/Coding_Ninjas/CN_Letter_Combination_of_a_Phone_Number.cpp
@@ -0,0 +1,24 @@
+
+    void solve(int index, string digits, string temp, vector<string> &res, string check[ ]){
+        if(index == digits.length()){
+            res.push_back(temp);
+            return;
+        }
+
+        int val = digits[index] - '0';
+        string str = check[val];
+
+        for(int i=0;i<str.length();i++){
+            temp.push_back(str[i]);
+            solve(index+1,digits,temp,res,check);
+            temp.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size() == 0) return {};
+        vector<string>res;
+        string temp= "";
+        string check[10] = {"", "" ,"abc", "def", "ghi", "jkl", "mno", "pqrs","tuv", "wxyz"};
+        solve(0,digits,temp,res, check);
+        return res;
+    }
